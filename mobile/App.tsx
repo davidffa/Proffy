845908c79ac5f3c48209af97b37bbc5c4dd99830
David <davidfilipeamorim@gmail.com>
2020-08-06 14:52:04 +0100

    Navigation of 'Landing Page'

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Landing from './src/pages/Landing';
+
 import { AppLoading } from 'expo';
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+
+import AppStack from './src/routes/AppStack';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -18,7 +20,7 @@ export default function App() {
   }else {
     return (
       <>
-        <Landing />
+        <AppStack />
         <StatusBar style="light" />
       </>
     );
