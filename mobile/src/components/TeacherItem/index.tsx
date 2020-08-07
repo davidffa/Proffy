d799a5cb6195eaa6119ae6e93be8a94f54fc0fd3
David <davidfilipeamorim@gmail.com>
2020-08-07 15:11:54 +0100

    Increment connections number when whatsapp button pressed

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -7,6 +7,8 @@ import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
+import api from '../../services/api';
+
 import styles from './styles';
 
 export interface Teacher {
@@ -28,6 +30,10 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
     const [isFavorited, setIsFavorited] = useState(favorited);
     
     function handleLinkToWhatsapp() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+
         Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
     }
 
