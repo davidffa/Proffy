e8f78c89fe7c82a73536e0304c6dda898874993c
David <davidfilipeamorim@gmail.com>
2020-08-11 16:11:13 +0100

    Login page connection with backend

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,12 +1,63 @@
-import React from 'react';
+import React, { FormEvent, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import bgImage from '../../assets/images/background-image.svg';
 import purpleHeart from '../../assets/images/icons/purple-heart.svg';
 import eye from '../../assets/images/icons/eye.svg';
+import eyeDivided from '../../assets/images/icons/eye-divided.svg';
 
 import './styles.css';
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [remember, setRemember] = useState(false);
+    const [visiblePassword, setVisiblePassword] = useState(false);
+
+    useEffect(() => {
+        const button = document.getElementById('submit-button');
+        if (!button) return;
+
+        let regExEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (regExEmail.test(email) && password.length >= 8) {
+            button.style.cursor = 'pointer';
+            button.style.backgroundColor = '#04D361'
+            button.style.color = '#FFF';
+        }else {
+            button.style.cursor = 'not-allowed';
+            button.style.backgroundColor = '#DCDCE5';
+            button.style.color = '#9C98A6';
+        }
+    }, [email, password]);
+
+    useEffect(() => {
+        const password = document.getElementById('password') as HTMLInputElement;
+
+        if (visiblePassword) {
+            password.type = 'text';
+        }else {
+            password.type = 'password';
+        }
+    }, [visiblePassword]);
+
+    async function handleLogin(e: FormEvent) {
+        e.preventDefault();
+
+        try {
+            const res = await api.post('login', {
+                email,
+                password
+            });
+
+            console.log(res.data);
+        }catch (err) {
+            alert('Email ou password incorretos!');
+        }
+    }
+
     return (
         <div id="login-container">
             <div className="img-container">
@@ -14,32 +65,72 @@ function Login() {
             </div>
             
             <div className="form-container">
-                <form className="login-form">
+                <form className="login-form" onSubmit={handleLogin}>
                     <fieldset>
                         <legend>Fazer login</legend>
 
-                        <input type="email" placeholder="E-mail" className="email-input" />
+                        <input 
+                            type="email" 
+                            placeholder="E-mail" 
+                            className="email-input"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            required
+                            autoComplete="on"
+                        />
                         
                         <div className="password">
-                            <input type="password" placeholder="Senha" className="password-input" />
-                            <img src={eye} alt="Mostrar" />
+                            <input 
+                                type="password" 
+                                placeholder="Senha" 
+                                className="password-input"
+                                id="password"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                                required
+                                minLength={8}
+                            />
+
+                            {!visiblePassword 
+                                ? (
+                                    <img 
+                                        src={eye} 
+                                        alt="Mostrar"
+                                        className="eye"
+                                        onClick={e => setVisiblePassword(true)}
+                                    />
+                                )
+                                : (
+                                    <img 
+                                        src={eyeDivided} 
+                                        alt="Esconder"
+                                        className="eyeDivided"
+                                        onClick={e => setVisiblePassword(false)}
+                                    />
+                                )
+                            }
+                            
                         </div>
                         
                         <div className="form-footer">
-                            <input type="checkbox" className="checkbox" />
+                            <input 
+                                type="checkbox" 
+                                className="checkbox"
+                                onClick={e => setRemember(!remember)}
+                            />
                             <label>Lembrar-me</label>
 
-                            <a className="forgot-password" href="forgotpassword">Esqueci minha senha</a>
+                            <Link className="forgot-password" to="forgotpassword">Esqueci minha senha</Link>
                         </div>
                     </fieldset>
 
-                    <button>Entrar</button>
+                    <button id="submit-button" type="submit">Entrar</button>
                 </form>
 
                 <footer>
                     <div className="no-acc">
                         <p>Não tem conta?</p>
-                        <a href="register">Registe-se</a>
+                        <Link to="register">Registe-se</Link>
                     </div>
                     
 
