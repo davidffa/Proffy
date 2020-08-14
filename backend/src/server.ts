b1a1af6f090d41fc82bf67795b767b966f8b87c8
David <davidfilipeamorim@gmail.com>
2020-08-14 15:47:39 +0100

    Add morgan middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 require('dotenv').config();
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import routes from './routes';
 import path from 'path';
 
@@ -8,7 +9,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes)
+app.use(morgan('tiny'));
+app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
