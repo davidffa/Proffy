b203cfb2850569586bd24915a08081f4f842f0c0
David <davidfilipeamorim@gmail.com>
2020-08-23 01:07:56 +0100

    Onboarding second page

--- mobile/src/pages/Onboarding/OnboardingSecond/index.tsx
+++ mobile/src/pages/Onboarding/OnboardingSecond/index.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+
+import studyIcon from '../../../assets/images/give-classes-onboarding.png';
+import onboardingBg from '../../../assets/images/backgrounds/secondOnboardingBg.png';
+import onboardingPages from '../../../assets/images/onboardingPages2.png';
+import front from '../../../assets/images/icons/front.png';
+
+import styles from './styles';
 
 function OnboardingSecond() {
-    return <View />
+    function handleNavigate() {
+
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.backgroundView}>
+                <Image 
+                    source={onboardingBg} 
+                    style={styles.onboardingBackground} 
+                />
+                <Image 
+                    source={studyIcon}
+                    style={styles.studyIcon}
+                />
+            </View>
+            <View style={styles.contentView}>
+                <Text style={styles.numberText}>02.</Text>
+                <Text style={styles.description}>Ou dê aulas sobre o que você mais conhece</Text>
+                
+                <View style={styles.footer}>
+                    <Image source={onboardingPages} />
+
+                    <TouchableOpacity onPress={handleNavigate}>
+                        <Image source={front} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    );
 }
 
 export default OnboardingSecond;
---
+++ mobile/src/pages/Onboarding/OnboardingSecond/styles.ts
@@ -0,0 +1,53 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f0f0f7',
+    },
+
+    backgroundView: {
+        height: '45%',
+        backgroundColor: '#04D361',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    onboardingBackground: {
+        position: 'absolute',
+    },
+
+    studyIcon: {
+        marginTop: 40
+    },
+
+    contentView: {
+        flex: 1,
+        paddingTop: 80,
+        paddingLeft: 40,
+        paddingRight: 40,
+    },
+
+    numberText: {
+        fontSize: 40,
+        fontFamily: 'Archivo_500Medium',
+        color: '#6A6180',
+        opacity: 0.16
+    },
+
+    description: {
+        marginTop: 30,
+        fontSize: 24,
+        fontFamily: 'Archivo_500Medium',
+        color: '#6A6180',
+        maxWidth: 190
+    },
+
+    footer: {
+        flexDirection: 'row',
+        marginTop: 70,
+        justifyContent: 'space-between'
+    }
+});
+
+export default styles;
